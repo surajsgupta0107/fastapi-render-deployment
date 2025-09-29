bb46b19a9c922c3a3b5cc8c40bc1ca55917c8377
surajsgupta0107 <surajsgupta0107@gmail.com>
2025-09-30 02:51:01 +0530

    using postgresql db

--- api/database/core.py
+++ api/database/core.py
@@ -2,9 +2,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "sqlite:///./mydatabase.db"
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+load_dotenv()
+
+# DATABASE_URL = "sqlite:///./mydatabase.db"
+# engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
+DATABASE_URL = os.environ.get("DATABASE_PATH")
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
